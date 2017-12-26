546d47570272eebd591d6a65a45fefdcb20e3b7c
elaygl <elaygl@gmail.com>
2017-12-26 14:39:13 +0200

    added basic version of the script

---
+++ index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const yaml = require('js-yaml');
+
+const envFileContent = fs.readFileSync('./.env', 'utf-8');
+
+const output = {
+  env: [],
+};
+envFileContent.split('\n').forEach(x => {
+  if (x.trim() === '') return;
+
+  const [name, value] = x.split('=');
+
+  output.env.push({
+    name,
+    value,
+  });
+});
+
+const result = yaml.safeDump(output)
+
+fs.writeFileSync('env.yaml', result);
+
+console.log('Done.');
